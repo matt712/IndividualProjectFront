bf254fa7fe7e623bb8a57f2790928ee566d35a44
matt712 <mzgadd@gmail.com>
2019-03-03 16:59:23 +0000

    Full CRUD for users and  notes

--- src/DeleteNoteButton.js
+++ src/DeleteNoteButton.js
@@ -4,6 +4,7 @@ import Axios from 'axios';
 class DeleteNoteButton extends Component{
     constructor(props){
         super(props);
+        this.state = {updating: false};
     }
     deleteNote =()=>{
        var url = `http://localhost:8080/IndividualProject/api/MatchUpNote/deleteMatchUpNote/${this.props.id}`
@@ -12,14 +13,44 @@ class DeleteNoteButton extends Component{
        }).catch(function(response){
             alert("Deletion failed, try again");
        });
-     }
+    }
+    handleChange = (e) =>{
+        this.setState({updating: true, contents: e.target.value});
+    }
+    updateNote = (e) =>{
+        if(this.state.updating===true){
+            e.preventDefault();
+            var url = `http://localhost:8080/IndividualProject/api/MatchUpNote/updateMatchupNote/${this.props.id}`;
+            var tempCont = this.state.contents;
+            Axios.put(url, tempCont).then(function(response){
+                alert("note created");
+                console.log(response.data);
+            }).catch(function(error){
+                alert("note creation failed");
+            });
+            this.setState({updating:false});
+        }else{
+            this.setState({updating:true});
+        }
+    }
     render(){
+        if(this.state.updating===false){
         return(
             <div>
             <button type="button" onClick={this.deleteNote}>Delete note</button>
             <button type="button" onClick={this.updateNote}>Update Note</button>
             </div>
-        )
+        )}else{
+            return(
+                <div>
+                    <form onSubmit={this.updateNote}>
+                    Note content: <br/>
+                    <input type="text" name="contents" onChange={this.handleChange}></input>
+                    <input type="submit" value="Submit"/>
+                </form>
+                </div>
+            )
+        }
     }
 }
 export default DeleteNoteButton;
